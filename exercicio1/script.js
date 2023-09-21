bb93ad5c08a98c85436fcb0e59db0cae6a13158b
Thiago Rosa <thiagorcon@hotmail.com>
2023-09-21 15:56:00 -0300

    envio exercicio1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,23 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+//cada array é um time e um indice é um jogador
+//let timesCamp = ['flamengo','vasco','fluminense','botafogo','bangu']
+
+for (let i = 0; i < array.length; i++) {
+    let jogador = array[i]
+    console.log(`jogador - ${i + 1} `);
+    for (let j = 0; j < jogador.length; j++) {
+          console.log(`temporada ${ j +1} ${jogador[j]}`);
+     }
+     console.log('-----------');
+}
+
+
+// for (const key in array) {
+//     //let soma = 1
+//     console.log(array[]);          
+//     //console.log(array[soma++]);            
+//         }
+
+
